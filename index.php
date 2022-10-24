7ec5c5d9e4ef489d6198f4969a2d66edcfb383aa
Ainur Rochmah <rochmahain@gmail.com>
2022-10-24 11:06:39 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+    echo "Hello Wordl! <br>";
+
+    $nama = "Ainur";
+    $umur = 20;
+
+    echo "Nama saya <strong>$nama,</strong> umur saya $umur tahun <br>";
+
+    $namaAdik = "Faris";
+    $umurAdik = 12;
+
+    echo "Nama adik saya <strong>$namaAdik,</strong> dia berusia $umurAdik tahun <br>";
+    
+    $selisihumur = $umur - $umurAdik;
+    $jumlahumur = $umur + $umurAdik;
+
+    echo "<strong>Selisih</strong> umur saya dan umur adik saya adalah $selisihumur tahun <br>";
+    echo "<strong>Jumlah</strong> umur saya dan umur adik saya adalah $jumlahumur tahun <br>";
+
+?>
